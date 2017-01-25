53c028ed30f6f1574b7609e3ede9ffa1c6625c6d
MaryiaChuhunova <Maryia_Chuhunova@epam.com>
2017-01-25 13:22:30 +0300

    Create Sum.java

---
+++ Task1.6/Sum.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+/**
+ * main class, contains entry point
+ * Created by Maryia_Chuhunova on 1/20/2017.
+ */
+public class Sum {
+    /**
+     * entry point
+     *
+     * @param args arguments from command line. if it's empty program asks user to enter data;
+     */
+    public static void main(String[] args) {
+
+        String[] sNumbers = getData(args);
+        if (validateData(sNumbers)) {
+            System.out.println((sum(getSides(sNumbers))));
+        }
+    }
+
+    /**
+     * involutes number into 2-nd degree if it's non-negative
+     *              and into 4-th degree if it's negative
+     *
+     * @param numbers integer array of numbers ;
+     * @return result of involution;
+     */
+    public static double sum(double[] numbers) {
+
+        double min = numbers[0];
+        double max = numbers[0];
+        for(int i = 1; i < 3; i++) {
+            if(numbers[i] > max) {
+                max = numbers[i];
+            }
+            if(numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min+max;
+    }
+
+
+    /**
+     * string array of numbers into integer array of numbers
+     *
+     * @param sNumbers is a number in string type
+     * @return array of integer numbers
+     */
+    public static double[] getSides(String[] sNumbers) {
+
+        double[] numbers = new double[3];
+        try {
+            numbers[0] = Integer.parseInt(sNumbers[0]);
+            numbers[1] = Integer.parseInt(sNumbers[1]);
+            numbers[2] = Integer.parseInt(sNumbers[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid data");
+        }
+
+        return numbers;
+    }
+
+    /**
+     * checks if entered data is numeric
+     *
+     * @param sNumbers entered string data
+     * @return true if entered data numeric
+     */
+    public static boolean validateData(String[] sNumbers) {
+
+        int[] numbers = new int[3];
+        try {
+            numbers[0] = Integer.parseInt(sNumbers[0]);
+            numbers[1] = Integer.parseInt(sNumbers[1]);
+            numbers[2] = Integer.parseInt(sNumbers[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid data");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * gets data depending on source
+     *
+     * @param args data from comand line
+     * @return data from comand line if it's not empty, otherwise asks user to enter data;
+     */
+    public static String[] getData(String[] args) {
+
+        String[] numbers = {"", ""};
+
+        if (args.length > 2) {
+            numbers = args;
+        } else {
+            System.out.println("Enter data");
+            Scanner sc = new Scanner(System.in);
+            String line = sc.nextLine();
+            numbers = line.split(" ");
+        }
+        return numbers;
+    }
+
+}
